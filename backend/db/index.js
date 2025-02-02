6c3f8826e83954ee63eec8bf11e1715dd9644505
RajaRuslito <litoraja123@gmail.com>
2025-02-02 21:29:14 +0700

    redeployment2

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,21 +1,21 @@
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  connectionString: 'postgresql://limto_owner:43jPCuJfwGmg@ep-soft-sky-a19448lp.ap-southeast-1.aws.neon.tech/limto?sslmode=require',
-  ssl: {
-    rejectUnauthorized: false, // ✅ This enables SSL for NeonDB
-  },
-});
-
-module.exports = pool;
 // const { Pool } = require('pg');
 
 // const pool = new Pool({
-//   user: 'postgres',
-//   host: 'localhost',
-//   database: 'jarvis_telkom',
-//   password: 'postgres',
-//   port: 5432,
+//   connectionString: 'postgresql://limto_owner:43jPCuJfwGmg@ep-soft-sky-a19448lp.ap-southeast-1.aws.neon.tech/limto?sslmode=require',
+//   ssl: {
+//     rejectUnauthorized: false, // ✅ This enables SSL for NeonDB
+//   },
 // });
 
 // module.exports = pool;
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'jarvis_telkom',
+  password: 'postgres',
+  port: 5432,
+});
+
+module.exports = pool;
